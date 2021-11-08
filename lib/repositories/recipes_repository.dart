efe23247f20eb5fc06e4f4ed199d3089ba47acca
Rodolfo <rluthen@gmail.com>
2021-11-07 18:06:28 -0600

    rename

--- lib/repositories/recipes_repository.dart
+++ lib/repositories/recipes_repository.dart
@@ -1,25 +1,26 @@
 import 'dart:convert';
 
+import 'package:diet_app/models/hit.dart';
 import 'package:diet_app/models/recipes.dart';
 import 'package:http/http.dart';
 
-class RecipeRepository {
-  static final RecipeRepository _RecipeRepository =
-      RecipeRepository._internal();
-  factory RecipeRepository() {
-    return _RecipeRepository;
+class RecipesRepository {
+  static final RecipesRepository _RecipesRepository =
+      RecipesRepository._internal();
+  factory RecipesRepository() {
+    return _RecipesRepository;
   }
-  RecipeRepository._internal();
+  RecipesRepository._internal();
 
   final String URL =
       "https://api.edamam.com/api/recipes/v2?app_id=8e34028a&app_key=5855870c7063ce2c4810878b6290b022&type=public&health=alcohol-cocktail&diet=balanced&q=steak";
 
-  Future<Recipes?> getRecipes() async {
+  Future<List<Hit>?> getRecipes() async {
     try {
       Response response = await get(Uri.parse(URL));
       if (response.statusCode == 200) {
-        Recipes recipes = Recipes.fromJson(jsonDecode(response.body));
-        return recipes;
+        Recipes recipesData = Recipes.fromJson(jsonDecode(response.body));
+        return recipesData.hits;
       } else {
         return null;
       }
