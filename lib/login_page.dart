256653b29a5ab1b608c630d169fe16ec17ed9f60
daniel_lerma_qtDev <daniel.lerma@qualytech.club>
2021-11-08 20:54:08 -0600

    merging with main

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,149 +1,247 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
-import 'package:diet_app/create_account.dart';
-import 'package:diet_app/home_navigation.dart';
+import 'package:diet_app/auth/user_auth_repository.dart';
+import 'package:diet_app/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+import 'bloc/login_bloc.dart';
+import 'form_body.dart';
+
+class LoginForm extends StatefulWidget {
+  LoginForm({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginFormState createState() => _LoginFormState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  String _email = '';
-  String _pwd = '';
-  Widget _buildEmailField() {
-    return TextFormField(
-      style: TextStyle(color: Colors.grey),
-      decoration: InputDecoration(
-        labelText: 'Email',
-        fillColor: Colors.grey[200],
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey.shade200),
-        ),
-      ),
-      validator: (String? value) {
-        if (value!.isEmpty) {
-          return 'Email is required';
-        }
-      },
-      onSaved: (String? value) {
-        _email = value!;
-      },
-    );
+class _LoginFormState extends State<LoginForm> {
+  final UserAuthRepository _auth = UserAuthRepository();
+  // inicializar instacia de login bloc
+  late LoginBloc _loginBloc;
+
+  // para poder agregar eventos al bloc
+  // al presionar los botones de login
+
+  bool _showLoading = false;
+  String email = '';
+  String pwd = '';
+
+  @override
+  void dispose() {
+    _loginBloc.close();
+    super.dispose();
   }
 
-  Widget _buildPwdField() {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: 'Password',
-        fillColor: Colors.grey[200],
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey.shade200),
-        ),
-      ),
-      validator: (String? value) {
-        if (value!.isEmpty) {
-          return 'Password is required';
-        }
-      },
-      onSaved: (String? value) {
-        _pwd = value!;
-      },
-    );
+  void _anonymousLogIn(bool _) {
+    print("anonimo");
+    // agregar evento al login bloc de manera anónima
+    _loginBloc.add(LoginWithAnonymousEvent());
   }
 
-  Widget _buildLoginHeading() {
-    return SizedBox(
-      // color: Colors.red,
-      width: 150,
-      height: 65,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Login',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-                fontSize: 30),
-          )
-        ],
-      ),
-    );
+  void _googleLogIn(bool _) {
+    // invocar al login de firebase con el bloc
+    // recodar configurar pantallad Oauth en google Cloud
+    print("google");
+    // agregar evento al login bloc
+    _loginBloc.add(LoginWithGoogleEvent());
+  }
+
+  void _facebookLogIn(bool _) {
+    // invocar al login de firebase con el bloc
+    print("facebook");
+    // agregar evento al login bloc
+    _loginBloc.add(LoginWithFacebookEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DietApp'),
-        backgroundColor: Colors.green[400],
-      ),
-      body: Container(
-          margin: EdgeInsets.all(24),
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _buildLoginHeading(),
-                SizedBox(
-                  height: 10,
-                ),
-                _buildEmailField(),
-                SizedBox(
-                  height: 10,
-                ),
-                _buildPwdField(),
-                // ignore: prefer_const_constructors
-                SizedBox(
-                  height: 25,
-                ),
-                SizedBox(
-                  // para aumentar tamaño, lo wrappeo con sizedbox
-                  height: 60,
-                  child: ElevatedButton(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    style: ElevatedButton.styleFrom(primary: Colors.green[300]),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => HomeNavigation(),
+    return Stack(
+      children: [
+        // stack background image
+        Container(
+          color: '#cce3de'.toColor(),
+        ),
+        // form content
+        // agregar bloc login provider
+        // agregar bloc login consumer
+        // revisar estados y retornar error o home page o login page
+        SafeArea(
+            // como un padding/margin al rededor del widget
+            child: BlocProvider(
+          create: (context) {
+            _loginBloc = LoginBloc();
+            return _loginBloc;
+          },
+          // snippet: blocc
+          child: BlocConsumer<LoginBloc, LoginState>(
+            // sirve para todo MENOS para retornar widgets
+            // consume al bloc provider
+            listener: (context, state) {
+              // si el estado emite error, se muestra
+              if (state is LoginErrorState) {
+                _showLoading = false; // ya no está cargando nada
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                        title: Text(
+                          'Error al hacer login',
                         ),
-                      );
-                      print('login');
-                    },
-                  ),
-                ),
-                ElevatedButton(
-                  child: Text(
-                    'Create account',
-                    style: TextStyle(fontSize: 15, color: Colors.grey[400]),
-                  ),
-                  style: ElevatedButton.styleFrom(primary: Colors.white),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => CreateAccount()),
-                    );
+                        content: Text('${state.error}:\n${state.code}'));
                   },
+                );
+              }
+            },
+            builder: (context, state) {
+              if (state is LoginSuccessState) {
+                return HomePage();
+              }
+              // si no es success, se muestra el form
+              return Scaffold(
+                resizeToAvoidBottomInset: false,
+                backgroundColor: '#cce3de'.toColor(),
+                body: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  'Ingresa a tu cuenta',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey[700],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 40),
+                              child: Container(
+                                padding: EdgeInsets.only(top: 30, left: 3),
+                                child: Column(
+                                  children: <Widget>[
+                                    // Form(
+                                    //   autovalidateMode:
+                                    //       AutovalidateMode.onUserInteraction,
+                                    //   child: ListView(
+                                    //     scrollDirection: Axis.vertical,
+                                    //     shrinkWrap: true,
+                                    //     padding: EdgeInsets.all(24),
+                                    //     children: [
+                                    //       TextFormField(
+                                    //         decoration: InputDecoration(
+                                    //           label: Text('Correo'),
+                                    //           border: OutlineInputBorder(
+                                    //             borderRadius:
+                                    //                 BorderRadius.circular(15),
+                                    //           ),
+                                    //           filled: true,
+                                    //         ),
+                                    //         validator: (value) {
+                                    //           if (value == null ||
+                                    //               value.isEmpty) {
+                                    //             return 'Ingresa el correo';
+                                    //           } else {
+                                    //             email = value;
+                                    //           }
+                                    //           return null;
+                                    //         },
+                                    //       ),
+                                    //       SizedBox(height: 24),
+                                    //       TextFormField(
+                                    //         obscureText: true,
+                                    //         enableSuggestions: false,
+                                    //         autocorrect: false,
+                                    //         decoration: InputDecoration(
+                                    //           label: Text('Contraseña'),
+                                    //           border: OutlineInputBorder(
+                                    //             borderRadius:
+                                    //                 BorderRadius.circular(15),
+                                    //           ),
+                                    //           filled: true,
+                                    //         ),
+                                    //         validator: (value) {
+                                    //           if (value == null ||
+                                    //               value.isEmpty) {
+                                    //             return 'Ingresa la contraseña';
+                                    //           } else {
+                                    //             pwd = value;
+                                    //           }
+                                    //           return null;
+                                    //         },
+                                    //       ),
+                                    //     ],
+                                    //   ),
+                                    // ),
+                                    // MaterialButton(
+                                    //   onPressed: () async {
+                                    //     dynamic res =
+                                    //         await _auth.anonymousSignIn();
+                                    //     if (res.emailVerified == null) {
+                                    //       print('Error');
+                                    //     } else {
+                                    //       print('res: ${res.emailVerified}');
+                                    //       print('User signed in');
+                                    //     }
+                                    //   },
+                                    //   minWidth: double.infinity,
+                                    //   height: 60,
+                                    //   color: '#6b9080'.toColor(),
+                                    //   elevation: 0,
+                                    //   shape: RoundedRectangleBorder(
+                                    //     // side: BorderSide(color: Colors.black),
+                                    //     borderRadius: BorderRadius.circular(50),
+                                    //   ),
+                                    //   child: Text(
+                                    //     'Ingresar',
+                                    //     style: TextStyle(
+                                    //       fontWeight: FontWeight.bold,
+                                    //       fontSize: 18,
+                                    //       color: Colors.white,
+                                    //     ),
+                                    //   ),
+                                    // ),
+                                    FormBody(
+                                      onGoogleLoginTap: _googleLogIn,
+                                      onAnonymousLoginTap: _anonymousLogIn,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          )),
+              );
+            },
+          ),
+        )),
+        // mostrar como círculo de loading
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: _showLoading ? CircularProgressIndicator() : Container(),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
-
-import 'package:diet_app/login_page.dart';
+import 'package:diet_app/auth/bloc/auth_bloc.dart';
+import 'package:diet_app/splash_screen.dart';
+import 'package:diet_app/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+import 'home/home_page.dart';
+
+void main() async {
+  // inicializar firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // auth bloc provider
+  runApp(BlocProvider(
+    create: (context) =>
+        AuthBloc()..add(VerifyAuthEvent()), // le agrega el evento
+    child: MyApp(), // todo myapp tiene acceso a la auth
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      // theme: ThemeData(primarySwatch: Colors.green),
-      home: LoginPage(),
+      home: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is AlreadyAuthState) {
+            // si ya está auth, vamos a homepage
+            return HomePage();
+          } else if (state is UnAuthState) {
+            // si no, a welcome page -> de aquí a login o create
+            return WelcomePage();
+          }
+          // por mientras...
+          return SplashScreen();
+        },
+      ),
     );
   }
 }
